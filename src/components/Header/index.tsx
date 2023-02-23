bde0ae5855d2867bbecf097e69b5d0ed64ac8f01
uleon <alessandrouleon85@gmail.com>
2023-02-22 21:06:14 -0400

    docs: coretion in active link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,20 @@
 import { HeaderContainer } from "./styles";
-import logoIgnite from '../../assets/Logo.svg'
-import {Timer, Scroll} from 'phosphor-react'
+import logoIgnite from "../../assets/Logo.svg";
+import { Timer, Scroll } from "phosphor-react";
 import { NavLink } from "react-router-dom";
 
 export function Header() {
-    return (
-        <HeaderContainer>
-           <img src={logoIgnite} alt="" />
-          <nav>
-            <NavLink to="/" title="Time"><Timer size={24}/></NavLink>
-            <NavLink to="/history" title="Histórico"><Scroll size={24}/></NavLink>
-            </nav>  
-        </HeaderContainer>
-    );
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="time">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
 }
